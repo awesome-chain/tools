c611444297157bb0613a95f6587ba97bd69fccc7
qinxiuchen <qinxiuchen@gmail.com>
2019-09-09 14:49:56 +0800

    add test case

--- tests/account/account_test.go
+++ tests/account/account_test.go
@@ -39,6 +39,25 @@ func TestKeyPair(t *testing.T) {
 	assert.Equal(t, pubKey0, pubKey, "retrieve public key from private key not matched")
 }
 
+func TestKeyPairWithVrfKey(t *testing.T) {
+	var pubKey [32]uint8
+	var prvKey [64]uint8
+	pubKey, prvKey = crypto.VRFKeygen()
+	t.Logf("public key is: [0x%s]", hex.EncodeToString(pubKey[:]))
+	t.Logf("private key is: [0x%s]", hex.EncodeToString(prvKey[:]))
+	t.Logf("address is: [0x%s]", hex.EncodeToString(pubKey[:]))
+	msg := []byte("hello world")
+	sign := crypto.ED25519Sign(crypto.PrivateKey(prvKey), msg)
+	valid := crypto.ED25519Verify(crypto.PublicKey(pubKey), msg, sign)
+	if !valid{
+		t.Error("check sign failed")
+		return
+	}
+	t.Logf("sign msg [%s], checked passed with referred public key", msg)
+	pubKey0 := crypto.RetrievePubkey(crypto.PrivateKey(prvKey))
+	assert.Equal(t, crypto.PublicKey(pubKey0), crypto.PublicKey(pubKey), "retrieve public key from private key not matched")
+}
+
 func TestPartKeys(t *testing.T){
 	var masterPubKey [32]byte
 	var masterPrvKey [64]byte
